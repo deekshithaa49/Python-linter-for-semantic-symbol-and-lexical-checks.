4e76bf63a93db32f15828918663526bfa1c25ff3
sharvanich <130927204+sharvanich@users.noreply.github.com>
2024-11-17 00:24:45 +0530

    Update input3.py

--- dummy/parser8/input3.py
+++ dummy/parser8/input3.py
@@ -2,16 +2,6 @@ def main():
   x=1
   a=3
   a=x*3+5
-  if x>a:
-    print('Hi!!')
-    a=x*3+100
-    if x>a:
-      print('Hi!!')
-      a=x*3+100
-    else:
-      x=a*3+100
-  else:
-    x=a*3+100
   return 
 
 if __name__=="__main__":
