35b7f648070d53f54928b7b55d1004d4c3308407
sharvanich <130927204+sharvanich@users.noreply.github.com>
2024-11-16 16:39:23 +0530

    input1.py

--- input1.py
+++ input1.py
@@ -1,16 +1,23 @@
-x=1
-y=10
-z=5
+def main():
+    a=0
+    x, y, z=1, 2, 3
+    x, y, z=3, 10, 5
 
-if(x>5):
+if x>5:
     for i in range(10):
         y=x+3
-        print('hello')
+        print('greetings!')
 else:
     idx=1
 
 for i in range(10):
-    print('hello world')
+    print('welcome to python!')
     x=int(input('enter x : '))
-    if(x>5):
-        print('hi')
+    if x>5:
+        print('hello there!')
+    for j in range(z):
+        a=1
+return 1
+
+if __name__=="__main__":
+    main()
