4d8c2545fb09c9d336bb373423ac0a54721fddc0
sharvanich <130927204+sharvanich@users.noreply.github.com>
2024-11-16 16:29:46 +0530

    input1.py

---
+++ input1.py
@@ -0,0 +1,16 @@
+x=1
+y=10
+z=5
+
+if(x>5):
+    for i in range(10):
+        y=x+3
+        print('hello')
+else:
+    idx=1
+
+for i in range(10):
+    print('hello world')
+    x=int(input('enter x : '))
+    if(x>5):
+        print('hi')
