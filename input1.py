d638499fb1aa17c21f412bd89b651f05c61837c2
sharvanich <130927204+sharvanich@users.noreply.github.com>
2024-11-23 11:14:46 +0530

    Update input1.py

--- input1.py
+++ input1.py
@@ -1,23 +1,7 @@
-def main():
-    a=0
-    x, y, z=1, 2, 3
-    x, y, z=3, 10, 5
-
-if x>5:
-    for i in range(10):
-        y=x+3
-        print('greetings!')
-else:
-    idx=1
-
-for i in range(10):
-    print('welcome to python!')
-    x=int(input('enter x : '))
-    if x>5:
-        print('hello there!')
-    for j in range(z):
-        a=1
-return 1
-
-if __name__=="__main__":
-    main()
+for i in range(20):
+	x=10
+	print('hello')
+for i in list:
+	y=10
+while x<4:
+	print('hi')
